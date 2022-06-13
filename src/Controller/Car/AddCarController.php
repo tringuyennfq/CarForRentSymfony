690d7b20e4d46e88a7d0c558f41ec10cd0ec670d
tringuyennfq <khai.tri@nfq.asia>
2022-06-13 10:47:02 +0700

    feat: reformating code

--- src/Controller/Car/AddCarController.php
+++ src/Controller/Car/AddCarController.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Car;
 
 use App\Entity\Car;
 use App\Form\AddCarType;
-
 use App\Service\CarService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,8 +18,8 @@ class AddCarController extends AbstractController
         $this->carService = $carService;
     }
 
-    #[Route('/addcar', name: 'app_add_car', methods: ['GET','POST'])]
-    public function show(Request $request): JsonResponse|Response
+    #[Route('/car/add', name: 'app_add_car', methods: ['GET','POST'])]
+    public function show(Request $request): Response
     {
         $car =  new Car();
         $addCarForm = $this->createForm(AddCarType::class, $car);
@@ -29,12 +27,13 @@ class AddCarController extends AbstractController
         if ($addCarForm->isSubmitted() && $addCarForm->isValid()) {
             $isAddCarSuccess =  $this->carService->addCar($car, $addCarForm->get('imagePath')->getData());
             if ($isAddCarSuccess != null) {
-                return $this->json(['Message'=>'Car created!'], Response::HTTP_CREATED);
+                $message = 'Car created successfully!';
             }
-            return $this->json(['Message'=>'Car not created'], Response::HTTP_BAD_REQUEST);
+            $message = 'Car not created!';
         }
         return $this->render('/add_car/index.html.twig', [
-            'addCarForm'=>$addCarForm->createView()
+            'addCarForm' => $addCarForm->createView(),
+            'message' => $message ?? '',
         ]);
     }
 }
--- src/Controller/Car/CarDetailsController.php
+++ src/Controller/Car/CarDetailsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Car;
 
 use App\Entity\Car;
 use App\Repository\CarRepository;
